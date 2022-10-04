47687eac98e622cafd0bd171cf761036124a39d9
Jon-pre <j-pre@online.no>
2022-10-04 19:21:00 +0200

    Lagt til store deler av DAL. Den bruker ikke Async helt ennå, skal få lagt det til etter hvert.

---
+++ ITPE3200-Prosjekt1/DAL/AksjeContext.cs
@@ -0,0 +1,15 @@
+﻿using ITPE3200_Prosjekt1.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace ITPE3200_Prosjekt1.DAL
+{
+    public class AksjeContext : DbContext
+    {
+        public AksjeContext(DbContextOptions<AksjeContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<Aksje> Akjser { get; set; }
+
+    }
+}
