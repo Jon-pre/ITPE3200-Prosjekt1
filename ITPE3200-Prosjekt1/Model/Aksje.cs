dd6d9d4038abdc4df7d248017b7990e827e45de0
Erebrias <70891750+Jon-pre@users.noreply.github.com>
2022-10-13 11:20:16 +0200

    Hei

--- ITPE3200-Prosjekt1/Model/Aksje.cs
+++ ITPE3200-Prosjekt1/Model/Aksje.cs
@@ -7,4 +7,13 @@
         public int pris { get; set; }
         public int prosent { get; set; }
     }
+
+
+    public class Konto
+    {
+        public int id { get; set; }
+        public string navn { get; set; }
+        public string land { get; set; }
+        public int kontobalanse { get; set; }
+    }
 }
