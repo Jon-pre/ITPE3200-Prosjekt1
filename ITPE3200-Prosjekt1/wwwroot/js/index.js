7ae1c2062d5e27241be6156f03d97beee85402e6
Erebrias <70891750+Jon-pre@users.noreply.github.com>
2022-10-13 13:26:01 +0200

    Nå kan aksjer og aksjekonto listes ut i html. På tide med Matte :D d:

--- ITPE3200-Prosjekt1/wwwroot/js/index.js
+++ ITPE3200-Prosjekt1/wwwroot/js/index.js
@@ -1,5 +1,6 @@
 ﻿$(function () {
     hentAlle();
+    hentAlleKontoer();
 });
 
 function hentAlle() {
@@ -18,11 +19,35 @@ function formaterAlle(aksjer) {
             "<td>" + aksje.navn + "</td>" +
             "<td>" + aksje.pris + "</td>" +
             "<td>" + aksje.prosent + "</td>" +
-            "<td> <a class='btn btn-primary' href='kjop.html?id=" + aksje.id+"'> Klikk meg</a ></td > " +
-            "<td> <a class='btn btn-danger' onclick='selgAksje("+ aksje.id + ")'>Klikk meg</a></td>" + 
+            "<td> <a class='btn btn-primary' href='kjop.html?id=" + aksje.id+"'>Kjøp</a ></td > " +
+            "<td> <a class='btn btn-danger' onclick='selgAksje("+ aksje.id + ")'>Selg</a></td>" + 
             "</tr>"
            
     }
     ut += "</table"
        $("#aksjer").html(ut);
+}
+
+function hentAlleKontoer() {
+    $.get("aksje/hentAlleKontoer", function (kontoer) {
+        formaterKontoer(kontoer);
+    })
+}
+
+
+
+function formaterKontoer(kontoer) {
+    ut2 = "<table class='table table-dark'>" +
+        "<tr><th>navn</th><th>land</th><th>KontoBalanse</th></tr>";
+
+    for (let konto of kontoer) {
+        ut2 +="<tr>"+
+            "<td>" + konto.navn + "</td>" +
+            "<td>" + konto.land + "</td>" +
+            "<td>" + konto.kontobalanse + "</td>" +
+            "</tr>";
+    }
+    ut2 += "</table>"
+
+    $("#konto").html(ut2);
 }
--- ITPE3200-Prosjekt1/wwwroot/js/kjop.js
+++ ITPE3200-Prosjekt1/wwwroot/js/kjop.js
@@ -1,5 +1,5 @@
 ﻿$(function () {
-
+    hentAlleKontoer()
     const id = window.location.search.substring(1);
     const url = "aksje/hent?" + id;
 
@@ -10,3 +10,35 @@
         $("#prosent").text(aksjer.prosent);
     });
 });
+
+
+function hentAlleKontoer() {
+    $.get("aksje/hentAlleKontoer", function (kontoer) {
+        formaterKontoer(kontoer);
+    })
+}
+
+
+
+function formaterKontoer(kontoer) {
+    ut = "<table class='table table-dark'>" +
+        "<tr>" +
+        "<th>navn</th><th>land</th><th>KontoBalanse</th>" +
+        "</tr>"
+
+    for (let konto of kontoer) {
+        ut += "<tr>";
+        ut += "<td>" + konto.navn + "</td>" +
+              "<td>" + konto.land + "</td>" +
+              "<td>" + konto.kontobalanse + "</td>" +
+              "</tr>"
+    }
+    ut += "</table>"
+    $("#konto").html(ut)
+}
+
+function kjopAksje() {
+    const id = window.location.search.substring(1);
+    const url = "aksje/hent?" + id;
+
+}
