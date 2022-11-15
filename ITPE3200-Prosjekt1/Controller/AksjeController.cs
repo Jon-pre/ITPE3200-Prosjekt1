0b8e7e32a6749cf4542e4a3abb3e830a1310ec84
Meron Berhe Kidane <s354594@oslomet.no>
2022-11-15 01:16:50 +0100

    lagt til feilhådtering

--- ITPE3200-Prosjekt1/Controller/AksjeController.cs
+++ ITPE3200-Prosjekt1/Controller/AksjeController.cs
@@ -4,6 +4,7 @@ using ITPE3200_Prosjekt1.Model;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,18 +15,26 @@ namespace ITPE3200_Prosjekt1.Controller
     public class AksjeController : ControllerBase
     {
         private readonly IAksjeRepo _db;
+
         private const string _loggetInn = "LoggetInn";
 
-        public AksjeController(IAksjeRepo db)
+
+        private ILogger<AksjeController> _log;
+
+        public AksjeController(IAksjeRepo db, ILogger<AksjeController> log) // legge til ILogger<AksjeController> log
         {
             _db = db;
+            _log = log;
         }
 
-        public async Task<List<Aksje>> hentAlle()
+        public async Task<ActionResult<Aksje>> hentAlle()
         {
-            return await _db.hentAlle();
+
+
+            List<Aksje> aksjeliste = await _db.hentAlle();
+            return Ok(aksjeliste);
         }
-        
+
         public async Task<List<Konto>> hentAlleKontoer()
         {
             return await _db.hentAlleKontoer();
@@ -33,27 +42,47 @@ namespace ITPE3200_Prosjekt1.Controller
 
         public async Task<Aksje> hent(int id)
         {
-          
             return await _db.hent(id);
         }
 
-        public async Task<bool> kjop(Konto konto)
+        public async Task<ActionResult> kjop(Konto konto)
         {
-            return await _db.kjop(konto);
+            bool returOk = await _db.kjop(konto);
+            if (!returOk)
+            {
+                _log.LogInformation("du kan ikke kjøpe");
+                return NotFound("du kan ikke kjøpe");
+            }
+            return Ok("du har logget inn");
         }
 
-        public async Task<bool> Endre(Konto konto)
+        public async Task<ActionResult> Endre(Konto konto)
         {
-            return await _db.Endre(konto);
+            bool returOk = await _db.Endre(konto);
+            if (!returOk)
+            {
+                _log.LogInformation("akjen ble ikke endret");
+                return NotFound("aksjen ble ikke endret");
+            }
+            return Ok("aksjen ble endret");
         }
 
         public async Task<Konto> hentKonto(int id)
         {
             return await _db.hentKonto(id);
         }
-        public async Task<bool> Slett(int id)
+
+        public async Task<ActionResult> Slett(int id)
         {
-            return await _db.Slett(id);
+
+            bool returOk = await _db.Slett(id);
+            if (!returOk)
+            {
+                _log.LogInformation("aksjen ble ikke slettet");
+                return NotFound("aksjen ble ikke slettet");
+            }
+
+            return Ok("aksjen ble slettet");
         }
         public async Task<ActionResult> logInn(Konto konto)
         {
